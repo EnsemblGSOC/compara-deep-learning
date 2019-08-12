cd3c529348570af9ccc8d722ef3713818f7662eb
HarshitGupta11 <50410275+HarshitGupta11@users.noreply.github.com>
2019-08-12 11:25:24 +0530

    Add files via upload

--- prepare_synteny_matrix.py
+++ prepare_synteny_matrix.py
@@ -1,69 +1,90 @@
-import pandas as pd 
 import numpy as np 
-import os 
-import sys
-import progressbar
+import pandas as pd 
 import json
-import gc
-from read_data import read_data_homology
+import os
+import sys
+import pickle
+from select_data import read_db_homology
+from threads import Procerssrunner
 from read_get_gene_seq import read_gene_sequences
-from create_synteny_matrix import synteny_matrix
-from access_data_rest import update_rest
-from select import select,create_map_reverse
+from access_data_rest import update_rest,update_rest_protein
+from process_negative import write_fasta
 
-if not os.path.isdir("processed/synteny_matrices"):
-    os.mkdir("processed/synteny_matrices")
+def read_data_synteny(nop,name):
+    smg=[]
+    sml=[]
+    indexes=[]
+    for i in range(nop):
+        try:
+            with open("temp_"+name+"/thread_"+str(i+1)+"_smg.temp","rb") as file:
+                smg=smg+pickle.load(file)
+            with open("temp_"+name+"/thread_"+str(i+1)+"_sml.temp","rb") as file:
+                sml=sml+pickle.load(file)
+            with open("temp_"+name+"/thread_"+str(i+1)+"_indexes.temp","rb") as file:
+                indexes=indexes+pickle.load(file)
+        except Exception as e:
+            print("Problem with thread",i+1,"detected for",name,e)
+            continue
+    print(len(indexes))
+    return smg,sml,indexes
 
-arg=sys.argv
-arg=arg[1:]
+def load_neighbor_genes():
+    with open("processed/neighbor_genes.json","r") as file:
+        lsy=dict(json.load(file))
+    print(len(lsy))
+    print("Neighbor Genes Loaded")
+    return lsy
 
-nos=int(arg[0])
+def read_data_homology(dirname):
+    lf=os.listdir(dirname)
+    if len(lf)==0:
+        print("No Files in the Directory!!!!!!!")
+        sys.exit(1)
+    a_h=[]
+    d_h=[]
+    for x in lf:
+        df,n=read_db_homology(dirname,x)
+        n=n.split()[0]
+        try:
+            indexes=np.load("processed/"+n+"_selected_indexes.npy")
+        except:
+            print("Incomplete data for:",n)
+        df=df.loc[indexes]
+        a_h.append(df)
+        d_h.append(n)
+    return a_h,d_h
 
-with open("dist_matrix","r") as file:
-    matrix=file.readlines()
-matrix=[x.split("\t") for x in matrix]
-matrix=[[float(y) for y in x] for x in matrix]
-matrix=np.array(matrix)
-with open("sp_names","r") as file:
-    dname=file.readlines()
-dname=[x.split("\n")[0] for x in dname]
-spnmap,nspmap=create_map_reverse(dname)
-
-lsy={}
-a_h,d_h=read_data_homology("data_homology")
-print(d_h)
-d_h=list(d_h.keys())
-d_h=[x.split()[0] for x in d_h]
-print("Homology Data Read")
-for i in range(len(a_h)):
-    a_h[i]=select(a_h[i],nos,matrix,spnmap,nspmap,d_h[i])
-    assert(len(a_h[i])==nos)
-gc.collect()
-print("Data Selected")
-
-with open("processed/neighbor_genes.json","r") as file:
-    lsy=dict(json.load(file))
-print(len(lsy))
-print("Neighbor Genes Loaded")
-
-gene_sequences={}
-gene_sequences=read_gene_sequences(a_h,lsy,"geneseq","gene_sequences")
-print("Gene Sequences Loaded")
-
-print("Going to update not found sequences:")
-gene_sequences=update_rest(gene_sequences)
-
-n=3
-ndir="processed/synteny_matrices/"
-nf1="synteny_matrices_global"
-nf2="synteny_matrices_local"
-nf3="indexes"
-for i in range(len(a_h)):
-    df=a_h[i]
-    print(len(df))
-    synteny_matrices_global,synteny_matrices_local,indexes=synteny_matrix(gene_sequences,df,lsy,n,0)
-    np.save(ndir+str(d_h[i])+"_"+nf1,synteny_matrices_global)
-    np.save(ndir+str(d_h[i])+"_"+nf2,synteny_matrices_local)
-    np.save(ndir+str(d_h[i])+"_"+nf3,indexes)
-    print(len(indexes))    
-print("Synteny Matrices Created Successfully :)")
+def main():
+    arg=sys.argv
+    nop=int(arg[-1])
+    n=3
+    a_h,d_h=read_data_homology("data_homology")
+    print("Data Read")
+    lsy=load_neighbor_genes()
+    gene_sequences=read_gene_sequences(a_h,lsy,"geneseq","gene_seq_positive")
+    gene_sequences=update_rest(gene_sequences,"gene_seq_positive")
+    print("Gene Sequences Loaded.")
+    if not os.path.isdir("processed/synteny_matrices"):
+        os.mkdir("processed/synteny_matrices")
+    ndir="processed/synteny_matrices/"
+    nf1="synteny_matrices_global"
+    nf2="synteny_matrices_local"
+    nf3="indexes"
+    for i in range(len(a_h)):
+        df=a_h[i]
+        part=len(df)//nop
+        pr=Procerssrunner()
+        pr.start_processes(nop,df,gene_sequences,lsy,part,n,d_h[i])
+        smg,sml,indexes=read_data_synteny(nop,d_h[i])
+        print(len(indexes))
+        np.save(ndir+str(d_h[i])+"_"+nf1,smg)
+        np.save(ndir+str(d_h[i])+"_"+nf2,sml)
+        np.save(ndir+str(d_h[i])+"_"+nf3,indexes)
+        a_h[i]=df.loc[indexes]
+    print("Synteny Matrices Created Successfully :)")    
+    protein_sequences=read_gene_sequences(a_h,lsy,"pro_seq","pro_seq_positive")
+    protein_sequences=update_rest_protein(protein_sequences,"pro_seq_positive")    
+    write_fasta(protein_sequences,"protein_seq_positive")
+    print("Protein Sequences Loaded.")
+if __name__=="__main__":
+    main()
