fa3c90d88c95ce8245e58c061eca2fa7a4282ac7
Aidan Marshall <a.marshall19@imperial.ac.uk>
2021-05-27 08:40:57 +0100

    Download script split into two files, one for finding the file links and one for performing the downloads. The snakefile has been adapted to incorporate both of these steps into the procedure

--- pipeline/scripts/ftpg.py
+++ pipeline/scripts/ftpg.py
@@ -2,28 +2,8 @@ from ftplib import FTP
 import progressbar
 import os
 import sys
-import urllib.request as urllib
 import requests
 import numpy as np
-
-def download_data(x, dir_name):
-    fname = x.split("/")[-1]
-    path = os.path.join(dir_name, fname)
-    urllib.urlretrieve(x, path)
-
-
-def get_data_file(file, dir):
-    if not os.path.isfile(file):
-        print("The specified file does not exist!!!")
-        sys.exit(1)
-
-    with open(file, "r") as f:
-        lf = f.read().splitlines()
-
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-    for x in progressbar.progressbar(lf):
-        download_data(x, dir)
 
 
 # This wil download all the fasta files for the coding sequences. To change the directory, change the argument in the get_data_file argument.
@@ -36,7 +16,7 @@ ftp = FTP(host)
 ftp.login(user, password)
 print("Connected to {}".format(host))
 base_link = "ftp://ftp.ensembl.org"
-find sequences of all the cds files
+# find sequences of all the cds files
 l = ftp.nlst("/pub/release-96/fasta")
 lt = []
 for x in progressbar.progressbar(l):
@@ -50,7 +30,7 @@ print("seq files links obtained.")
 np.savetxt("seq_link.txt", np.array(lt), fmt="%s")
 print("seq_link.txt written")
 
-find all the files with protein sequences
+# find all the files with protein sequences
 print("Find all fasta files")
 l = ftp.nlst("/pub/release-96/fasta")
 lt = []
@@ -80,8 +60,3 @@ print("gtf files links obtained.")
 np.savetxt("gtf_link.txt", np.array(lt), fmt="%s")
 print("gtf_link.txt written")
 
-print("Downloading Data")
-get_data_file("gtf_link.txt", "data")
-get_data_file("seq_link.txt", "geneseq")
-get_data_file("protein_seq.txt", "pro_seq")
-print("Download Complete.................")
