51a658463f77ea7a38e8197dbda78a62fa95ca47
Aidan Marshall <a.marshall19@imperial.ac.uk>
2021-05-29 09:50:25 +0100

    Added directed output to the create_genome_maps.py script

--- pipeline2/scripts/create_genome_map/create_genome_maps.py
+++ pipeline2/scripts/create_genome_map/create_genome_maps.py
@@ -1,13 +1,16 @@
 import pickle
 from get_data import get_data_genome
 import sys
+
 # read in the input directory
 dir_g = sys.argv[1]
+out_path = sys.argv[2]
+
 cmap, cimap, ld, ldg, a, d = get_data_genome(dir_g)
 
 data = dict(cmap=cmap, cimap=cimap, ld=ld, ldg=ldg, a=a, d=d)
 
-with open("genome_maps", "wb") as file:
+with open(out_path, "wb") as file:
     pickle.dump(data, file)
 
 print("Genome Maps Created Successfully.")
