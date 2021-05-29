b6263c0ae720127a7244a63f80b5d78bd9cdb587
Aidan Marshall <a.marshall19@imperial.ac.uk>
2021-05-29 04:49:20 +0100

    Edited create_genome_maps.py to take input_dir as arg. This is run from snakefile

--- pipeline2/scripts/create_genome_map/create_genome_maps.py
+++ pipeline2/scripts/create_genome_map/create_genome_maps.py
@@ -1,7 +1,8 @@
 import pickle
 from get_data import get_data_genome
-
-dir_g = "data"
+import sys
+# read in the input directory
+dir_g = sys.argv[1]
 cmap, cimap, ld, ldg, a, d = get_data_genome(dir_g)
 
 data = dict(cmap=cmap, cimap=cimap, ld=ld, ldg=ldg, a=a, d=d)
