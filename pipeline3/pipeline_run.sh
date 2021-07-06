edf32c92d9c9bfa8f12155c5b7ffd0fa556ca9b1
Aidan Marshall <a.marshall19@imperial.ac.uk>
2021-07-06 12:17:13 +0100

    pipeline sub updated

--- pipeline3/pipeline_run.sh
+++ pipeline3/pipeline_run.sh
@@ -1,8 +1,8 @@
 pipeline_dir="/hps/software/users/ensembl/repositories/compara/amarshall/compara-deep-learning/pipeline3"
+echo $pipeline_dir
 
 export OMP NUM THREADS=6
 bsub -n 6 <your code>
 
 snakemake --dag | dot -Tpng > dag.png
-snakemake -s snakefile --use-conda --cluster \
-'bsub -e $pipeline_dir -o $pipeline_dir -n {threads} -M {resources.memory}' --jobs 10
+snakemake -s snakefile --use-conda --cluster 'bsub -e $pipeline_dir -o $pipeline_dir -n {threads} -M {resources.memory}' --jobs 10
