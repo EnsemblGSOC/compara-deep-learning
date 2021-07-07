c465007be822a82876923c00e00e5e5473f99d6b
Aidan Marshall <a.marshall19@imperial.ac.uk>
2021-07-07 11:02:50 +0100

    updated the pipeline executor to have more latency as snakemake was missing output files that were actually present

--- pipeline3/pipeline_run.sh
+++ pipeline3/pipeline_run.sh
@@ -3,4 +3,4 @@ echo $pipeline_dir
 
 export OMP NUM THREADS=16
 snakemake --dag | dot -Tpng > dag.png
-snakemake -s snakefile --use-conda --cluster 'bsub -e $pipeline_dir -o $pipeline_dir -n {threads} -M {resources.memory}' --jobs 10
+snakemake -s snakefile --use-conda --cluster 'bsub -e $pipeline_dir -o $pipeline_dir -n {threads} -M {resources.memory}' --jobs 100 --latency-wait 30
