eccb396d7c818bcf36616dd703760aebdea4ab69
Aiden Marshall <amarshall@codon-login-02.ebi.ac.uk>
2021-07-13 12:49:55 +0100

    Prototyped faster pipeline complete. Fast simpling of both homologs and paralogs. Algorithm for picking non-homologous pairs betweeen ttwo genes.  Rapid Smith-Waterman alignments to make data generation rapid. Full PFAM matrices and

---
+++ pipeline3/scripts/Pfam_matrix.py
@@ -0,0 +1,171 @@
+from skbio import Protein
+from skbio.alignment import local_pairwise_align_protein, global_pairwise_align_protein
+from Bio import SeqIO
+import json
+import pandas as pd
+import numpy as np
+import progressbar
+import edlib as ed
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument( 
+    "--species",
+    help="Species data to process",
+)
+parser.add_argument( 
+    "--samples_dir",
+    help="Directory containing all neighbourhood data",
+)
+parser.add_argument( 
+    "--Pfam_dir",
+    help="directory containing pfam alignments",
+)
+parser.add_argument( 
+    "--out_dir",
+    help="output directory for syneny matrices",
+)
+args = parser.parse_args()
+
+
+
+
+samples_df = pd.read_csv(args.samples_dir + "/" + args.species + "_samples_pairs.csv")
+
+# read in the samples for both the species and the candidate homologies
+with open( args.samples_dir + "/" + args.species + "_sample_neighbour.json","r") as f:
+    sample_neighbours = json.load(f)
+    
+with open( args.samples_dir + "/" + args.species + "_homology_sample_neighbour.json","r") as f:
+    sample_homology_neighbours = json.load(f)
+
+gene_series = samples_df.gene_stable_id.map(sample_neighbours)
+gene_series[gene_series.isna()] = gene_series[gene_series.isna()].apply(lambda x: [[0,0,0],[0,0,0]]) # fill the null values
+array1 = np.concatenate(gene_series.apply( np.array ).values ).reshape((-1,6))
+array1 = np.concatenate((samples_df.gene_stable_id.values[:,np.newaxis], array1), axis=1) #.reshape(1,-1)
+
+gene_series = samples_df.homology_gene_stable_id.map(sample_homology_neighbours)
+gene_series[gene_series.isna()] = gene_series[gene_series.isna()].apply(lambda x: [[0,0,0],[0,0,0]]) # fill the null values
+array2 = np.concatenate(gene_series.apply( np.array ).values ).reshape((-1,6))
+array2 = np.concatenate((samples_df.homology_gene_stable_id.values[:,np.newaxis], array2), axis=1) #.ravel()[np.newaxis,:]
+
+# array1 = pd.Series(array1[0])
+
+# array2 = pd.Series(array2[0])
+
+keys = "qseqid qlen sseqid slen qstart qend sstart send".split(" ")
+
+keys = ['qseqid', 'qlen', 'sseqid', 'slen', 'qstart', 'qend', 'sstart', 'send']
+
+pfam = pd.read_csv(args.Pfam_dir + "/" + args.species + ".outs.tsv", delimiter="\t", names=keys)
+
+pfam["qseqid"] = pfam["qseqid"].str.split(".").str[0]
+
+pfam_dfs = []
+for query_species in samples_df.homology_species.drop_duplicates():
+    temp_pfam = pd.read_csv( args.Pfam_dir + "/" + query_species + ".outs.tsv", delimiter="\t", names=keys)
+    temp_pfam["qseqid"] = temp_pfam["qseqid"].str.split(".").str[0]
+    pfam_dfs.append(temp_pfam)
+    # pfam_map.update(temp_pfam.groupby("qseqid")["sseqid"].apply(list).to_dict())
+    
+
+pfam_df = pd.concat(pfam_dfs)
+
+import pyranges as pr
+
+
+pfam_array = np.zeros(shape=(array1.shape[0],7,7))
+
+
+for i,j,k in progressbar.progressbar(np.ndindex((array1.shape[0],7,7))):
+    """
+    Sometimes the genes aren't present in the pfam list. This shouldn't be possible as the queried genes and the
+    and the Diamond alignments both come from the longest_pep_fasta file
+    """
+    if (type(array1[i,j]) != str) or (type(array2[i,k]) != str):
+        pfam_array[i,j,k] = float("NaN")
+        continue
+    gene1 = array1[i,j]
+    gene2 = array2[i,k]
+    temp_df1 = pfam[pfam.qseqid == gene1]
+    temp_df2 = pfam_df[pfam_df.qseqid == gene2]
+    
+    if (temp_df1.shape[0] == 0) or (temp_df2.shape[0] == 0):
+        pfam_array[i,j,k] = float("NaN")
+        continue
+    else:
+        """
+        Pyranges requires the chromosome term, and cannot be changed.
+        Mentally read Pfam domain, instead of chromosome!
+        """   
+        
+        temp_df1 = temp_df1[["sseqid", "sstart", "send"]].rename(
+            columns={"sseqid": "Chromosome", "sstart": "Start", "send": "End"}
+        )
+        temp_df2 = temp_df2[["sseqid", "sstart", "send"]].rename(
+            columns={"sseqid": "Chromosome", "sstart": "Start", "send": "End"}
+        )
+        print("df shapes: ")
+        print(temp_df1.shape,temp_df2.shape)
+        ranges_1 = pr.PyRanges(temp_df1)
+        ranges_2 = pr.PyRanges(temp_df2)
+        print(ranges_1, ranges_2)
+        # Package them into a dictionary
+        grs = {n: m for n, m in zip(["ranges_1", " ranges_2"], [ranges_1, ranges_2])}
+        #  Check for sequence aligmnets overlapping on a Pfam domain
+        overlaps = pr.count_overlaps(grs).df
+        # Mark the overlaps
+        overlaps["overlap_bool"] = (overlaps[["ranges_1", " ranges_2"]].sum(axis=1) > 1) + 0
+        # Get the total number of actually overlapping Pfam domains
+        overlapped_pfams = (overlaps.groupby("Chromosome").sum()["overlap_bool"] > 0).sum()
+        # Get the total number of possible Pfam overlaps; ie total across both genes
+        total_pfams = overlaps["Chromosome"].drop_duplicates().shape[0]
+        # print(overlapped_pfams / total_pfams)
+        pfam_array[i,j,k] = (overlapped_pfams / total_pfams)
+
+np.savetxt(args.out_dir + "/" + args.species + "_Pfam.txt", pfam_array, fmt="%s")
+
+
+# pfam_values = []
+# print(array1, array2)
+# for gene1, gene2 in progressbar.progressbar(zip(array1, array2)):
+#     temp_df1 = pfam[pfam.qseqid == gene1]
+#     temp_df2 = pfam_df[pfam_df.qseqid == gene2]
+#     """
+#     Sometimes the genes aren't present in the pfam list. This shouldn't be possible as the queried genes and the
+#     and the Diamond alignments both come from the longest_pep_fasta file
+#     """
+    
+#     if temp_df1.empty or temp_df2.empty:
+#         pfam_values.append(float("NaN"))
+#     else:
+#         """
+#         Pyranges requires the chromosome term, and cannot be changed.
+#         Mentally read Pfam domain, instead of chromosome!
+#         """
+#         temp_df1 = temp_df1[["sseqid", "sstart", "send"]].rename(
+#             columns={"sseqid": "Chromosome", "sstart": "Start", "send": "End"}
+#         )
+#         temp_df2 = temp_df2[["sseqid", "sstart", "send"]].rename(
+#             columns={"sseqid": "Chromosome", "sstart": "Start", "send": "End"}
+#         )
+#         ranges_1 = pr.PyRanges(temp_df1)
+#         ranges_2 = pr.PyRanges(temp_df2)
+#         # Package them into a dictionary
+#         grs = {n: m for n, m in zip(["ranges_1", " ranges_2"], [ranges_1, ranges_2])}
+#         #  Check for sequence aligmnets overlapping on a Pfam domain
+#         overlaps = pr.count_overlaps(grs).df
+#         # Mark the overlaps
+#         overlaps["overlap_bool"] = (overlaps[["ranges_1", " ranges_2"]].sum(axis=1) > 1) + 0
+#         # Get the total number of actually overlapping Pfam domains
+#         overlapped_pfams = (overlaps.groupby("Chromosome").sum()["overlap_bool"] > 0).sum()
+#         # Get the total number of possible Pfam overlaps; ie total across both genes
+#         total_pfams = overlaps["Chromosome"].drop_duplicates().shape[0]
+#         print(overlapped_pfams / total_pfams)
+#         pfam_values.append(overlapped_pfams / total_pfams)
+
+
+# pfam_values = np.array(pfam_values).reshape(-1,7)
+
+# np.savetxt(args.out_dir + "/" + args.species + "_Pfam.txt", pfam_values, fmt="%s")
--- pipeline3/scripts/sample_gene.py
+++ pipeline3/scripts/sample_gene.py
@@ -4,6 +4,8 @@ import argparse
 import os
 import json
 
+np.random.seed(42)
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--homo", help="homology database path")
 parser.add_argument(
@@ -66,7 +68,14 @@ If dissatisfied, right your own rule to sample target species
 # look at species pairs based on the rank of their distance value
 # get equally spaced rank increments
 distance_increment = int(distance_df.shape[0] / args.n_species)
+# sometimes there are less species than the desired number
+if distance_df.shape[0] <= args.n_species:
+    distance_increment = 1
+print("Distance increment: ")
+print(distance_increment)
 # get the rank numbers
+print("Number of species in the distance matrix")
+print(distance_df.shape[0])
 distance_ranks = np.arange(1, int(distance_df.shape[0]), distance_increment)
 # get the species names
 query_species = distance_df[args.species].sort_values()[distance_ranks].index.values
@@ -90,12 +99,22 @@ homo_type_map = {
 homology["ortho_para"] = homology.homology_type.map(homo_type_map)
 # sample equal numbers of orthologs and paralogs for each of the queary species
 print(query_species)
-num_sub_samples = int(args.n_samples / (args.n_species * 2))
-samples_df = (
-    homology[homology.homology_species.isin(query_species)]
-    .groupby(["homology_species", "ortho_para"])
-    .sample(num_sub_samples)
+num_ortho_sub_samples = int(args.n_samples / (args.n_species * 2)) # factor 1/2 as half data should be ortholog.
+ortho_samples_df = (
+    homology[homology.homology_species.isin(query_species) & (homology.ortho_para == "ortholog")]
+    .groupby("homology_species")
+    .sample(num_ortho_sub_samples)
 )
+
+print(ortho_samples_df.shape[0])
+
+num_para_samples = ortho_samples_df.shape[0] # get the same number of paralogs as orthologs
+para_samples_df = (
+    homology[homology.ortho_para == "paralog"].sample(num_para_samples)
+)
+
+samples_df = pd.concat([ortho_samples_df,para_samples_df])
+
 
 samples_df.to_csv(args.out + "/" + args.species + "_samples_pairs.csv")
 
@@ -110,12 +129,12 @@ print("loading neighbourhood genes")
 with open(args.neighbours + "/" + args.species + "_all_neighbours.json") as f:
     neighbour_map = json.load(f)
 
-# neighbour_genes = dict(
-#     zip(samples_df.gene_stable_id, samples_df.gene_stable_id.map(neighbour_map))
-# )
-# print("writing pickle file: " + args.out + "/" + args.species + "_sample_neighbour.txt")
-# with open(args.out + "/" + args.species + "_sample_neighbour.json", "w") as json_file:
-#     json.dump(neighbour_genes, json_file)
+neighbour_genes = dict(
+    zip(samples_df.gene_stable_id, samples_df.gene_stable_id.map(neighbour_map))
+)
+print("writing pickle file: " + args.out + "/" + args.species + "_sample_neighbour.txt")
+with open(args.out + "/" + args.species + "_sample_neighbour.json", "w") as json_file:
+    json.dump(neighbour_genes, json_file)
 
 # read in the list of paths to homology species
 homo_paths = pd.Series(np.loadtxt(args.homo_paths, dtype="str"))
@@ -125,35 +144,124 @@ def homo_path_map(species):
     return homo_paths[homo_paths.str.contains(species)].values[0]
 
 
-# loop throug the homolog species to generate neighbours
-species_neighbour_dict = {}
-homology_neighbour_dict = {}
-for species in query_species:
-    # get only samples from that species
-    species_samples = samples_df[samples_df.homology_species == species]
-    neighbour_genes = dict(zip(species_samples.gene_stable_id, species_samples.gene_stable_id.map(neighbour_map)))
-    species_neighbour_dict.update(neighbour_genes)
-    # read in the query species neighbour list
+# loop through the homolog species to generate neighbours
+super_homology_neighbour_map = {}
+negs_main_species = []
+negs_query_species = []
+for species in pd.Series(query_species).append(pd.Series(args.species)):
+    print("loading homology neighbourhood genes")
     with open(args.neighbours + "/" + species + "_all_neighbours.json") as f:
         homology_neighbour_map = json.load(f)
-    # get the dictionary of each homolog gene and it's neighbours
-    homology_neighbour_genes = dict(
-        zip(
-            species_samples.homology_gene_stable_id,
-            species_samples.homology_gene_stable_id.map(homology_neighbour_map),
-        )
-    )
-    print("species: " + species)
+    super_homology_neighbour_map.update(homology_neighbour_map)
+    # select negative samples
+    """
+    this part of the for loop deals with obtaining negative samples using negative log.
+    First sample the genes from the main species. Then for those genes not in the homolgy database, 
+    sample from the query species randomly because there's no risk of overlap.
+    For the genes sampled from the main species that are in the homology database, use funky logic
+    to ensure you don't pick homologs from the query species
+    """
+    temp_df = homology[homology.homology_species == species] # only look at the current species
+    query_series = pd.Series(homology_neighbour_map.keys()) # get all genes from the query species
+    negative_samples = pd.Series(list(neighbour_map.keys())).sample(int(args.n_samples/2)) # sample them
+    temp = negative_samples.to_frame("gene_stable_id")
+    temp["species"] = species
+    negs_main_species.append(temp)
+    non_homo_samples_array = np.array(negative_samples) # initialise array for non-homologous pairs
+    index = ~negative_samples.isin(temp_df.gene_stable_id) # indices of samples not in the homology db
+    non_homo_samples_array[index] = query_series.sample(index.sum(), replace=True) # sample any genes for those indices
 
-    # append to the homology_neighbours dict
-    homology_neighbour_dict.update(homology_neighbour_genes)
+    def sample(gene):
+        """
+        Helper function to change the dataframe
+        """
+        bad_genes = temp_df[temp_df.gene_stable_id == gene].groupby("gene_stable_id")["homology_gene_stable_id"].apply(list)
+        return list(query_series[~query_series.isin(bad_genes)].sample(1))[0]
 
-with open(args.out + "/" + args.species + "_sample_neighbour.json", "w") as json_file:
-    json.dump(species_neighbour_dict, json_file)
+    non_homo_samples_array[~index] = negative_samples[~index].apply(sample) # get samples for the rest of the positions
+    temp = pd.DataFrame(non_homo_samples_array,columns=["non_homo_gene_stable_id"])
+    temp["non_homo_species"] = species
+    negs_query_species.append(temp)
 
+
+# save the positive example homologous pairs
+homology_neighbour_genes = dict(
+    zip(samples_df.homology_gene_stable_id, samples_df.homology_gene_stable_id.map(super_homology_neighbour_map))
+)
 
 with open(
     args.out + "/" + args.species + "_homology_sample_neighbour.json", "w"
 ) as json_file:
-    json.dump(homology_neighbour_dict, json_file)
+    json.dump(homology_neighbour_genes, json_file)
 
+
+# Deal with the negative examples
+negs_main_species = pd.concat(negs_main_species)
+negs_query_species = pd.concat(negs_query_species)
+
+pd.concat([negs_main_species.reset_index(), negs_query_species.reset_index()], axis=1).to_csv(args.out + "/" + args.species + "_negative_samples_pairs.csv")
+
+
+negative_neighbour_genes = dict(
+    zip(negs_main_species.gene_stable_id, negs_main_species.gene_stable_id.map(neighbour_map))
+)
+with open(args.out + "/" + args.species + "_negative_sample_neighbour.json", "w") as json_file:
+    json.dump(negative_neighbour_genes, json_file)
+
+negative_homology_neighbour_genes = dict(
+    zip(negs_query_species.non_homo_gene_stable_id, negs_query_species.non_homo_gene_stable_id.map(super_homology_neighbour_map))
+)
+
+with open(
+    args.out + "/" + args.species + "_negative_homology_sample_neighbour.json", "w"
+) as json_file:
+    json.dump(negative_homology_neighbour_genes, json_file)
+
+
+    
+"""
+ZOMBIE CODE. IGNORE
+"""
+
+# homology_neighbour_dict = {}
+# for species in query_species:
+#     # get only samples from that species
+#     species_samples = samples_df[samples_df.homology_species == species]
+#     neighbour_genes = dict(zip(species_samples.gene_stable_id, species_samples.gene_stable_id.map(neighbour_map)))
+#     species_neighbour_dict.update(neighbour_genes)
+#     # read in the query species neighbour list
+#     with open(args.neighbours + "/" + species + "_all_neighbours.json") as f:
+#         homology_neighbour_map = json.load(f)
+#     # get the dictionary of each homolog gene and it's neighbours
+#     homology_neighbour_genes = dict(
+#         zip(
+#             species_samples.homology_gene_stable_id,
+#             species_samples.homology_gene_stable_id.map(homology_neighbour_map),
+#         )
+#     )
+#     print("species: " + species)
+
+#     # append to the homology_neighbours dict
+#     homology_neighbour_dict.update(homology_neighbour_genes)
+
+# with open(args.out + "/" + args.species + "_sample_neighbour.json", "w") as json_file:
+#     json.dump(species_neighbour_dict, json_file)
+
+
+# with open(
+#     args.out + "/" + args.species + "_homology_sample_neighbour.json", "w"
+# ) as json_file:
+#     json.dump(homology_neighbour_dict, json_file)
+
+# # get negative samples
+# for species in query_species:
+#     with open(args.neighbours + "/" + species + "_all_neighbours.json") as f:
+#         homology_neighbour_map = json.load(f)
+#     negative_samples = pd.Series(list(neighbour_map.keys())).sample(num_sub_samples) # select genes
+#     negative_samples_array = np.array(negative_samples) # bad initialisation should be changed, to be more readable
+#     query_series = pd.Series(negative_neighbour_map.keys())
+#     negative_samples[~negative_samples.isin(homo.gene_stable_id)] = query_series.sample((~negative_samples.isin(homo.gene_stable_id))).values
+#         def get_samples(gene_list):
+#         non_list = pd.Series(gene_list)
+#         return query_series[~query_series.isin(non_list)].sample(1)
+#     negative_samples[~negative_samples.isin(homo.gene_stable_id)] = 
